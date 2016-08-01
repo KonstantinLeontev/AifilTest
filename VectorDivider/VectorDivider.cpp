13ce8d4be8fb75aaf5138f9b535a31472d8563ce
Konstantin Leontev <leokon@bk.ru>
2016-08-02 00:26:49 +0300

    Vector divider added

---
+++ VectorDivider/VectorDivider.cpp
@@ -0,0 +1,78 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void vectorDivider(int& N, int& M)
+{
+	cout << "Vector N = " << N << " / M = " << M << ": " << endl;
+	int remainder = N%M; // check for remainder
+	if (remainder == 0) // if all parts are equal
+	{
+		int part = N / M;
+		int counter = 1;
+		for (int i = 0; i < N; i += part)
+		{
+			cout << "Part " << counter << ": " << i << ", " << i + part - 1 << endl;
+			counter++;
+		}
+	}
+	else // if parts are not equal
+	{
+		int part = (N - remainder) / M;
+		int first{ 0 }, last{ 0 };
+		if (remainder > 1) // then there are two parts
+		{
+			first = remainder / 2;
+			cout << "First part length: " << first << endl; // test line
+			last = remainder - first;
+			cout << "Last part length: " << last << endl; // test line
+		}
+		else // then there is only one part
+		{
+			first = 1;
+		}		
+		cout << "Part first: " << 0;
+		if (first > 1)
+		{
+			cout << ", " << first;
+		}
+		cout << endl;
+		int counter = 1;
+		for (int i = first; i < N - last; i += part) // iterate through equals
+		{
+			cout << "Part " << counter << ": " << i << ", " << i + part - 1 << endl;
+			counter++;
+		}
+		if (last == 1) // if last exist and it's 0
+		{
+			cout << "Part last: " << N - last << endl;;
+		}
+		else if (last > 1) // if last exist and greater than 1
+		{
+			cout << "Part last: " << N - last << ", " << N - 1  << endl;
+		}		
+	}
+}
+
+int main()
+{
+	while (1)
+	{
+		int N, M;
+		cout << "Enter the size of vector (4 .. N) or 0 to exit: ";
+		cin >> N;
+		if (N == 0)
+		{
+			break;
+		}
+		cout << "Enter the number of parts (2 .. M) or 0 to exit: ";
+		cin >> M;
+		if (M == 0)
+		{
+			break;
+		}
+		vectorDivider(N, M);
+	}
+
+	return 0;
+}
